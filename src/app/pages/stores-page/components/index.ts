74470ab84425468b18b80fa05beb06c264539f09
Andrey Rybak <aliester.crowly@gmail.com>
2023-11-06 09:59:01 +0200

    refactoring stores page

--- src/app/pages/stores-page/components/index.ts
+++ src/app/pages/stores-page/components/index.ts
@@ -1,3 +1,2 @@
-export * from './store/store.component';
-export * from './store-list/store-list.component';
-export * from './store-widget/store-widget.component';
+export * from './stores-sidebar';
+export * from './store-widget';
